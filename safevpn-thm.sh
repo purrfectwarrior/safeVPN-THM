750f7972d87144a55d824983bf631565214fd510
purrfectwarrior <marbelvega565@gmail.com>
2024-12-23 07:51:04 -0400

    Update safevpn-thm.sh
    Se adiciono el instalador

--- safevpn-thm.sh
+++ safevpn-thm.sh
@@ -2,6 +2,42 @@
 
 # Author: Nisrin Ahmed aka Wh1teDrvg0n
 
+# Función para verificar si iptables está instalado
+check_iptables_installed() {
+    if command -v iptables >/dev/null 2>&1; then
+        echo "iptables ya está instalado."
+        exit 0
+    fi
+}
+
+# Función para instalar iptables en sistemas basados en Debian
+install_iptables_debian() {
+    sudo apt-get update
+    sudo apt-get install -y iptables
+}
+
+# Función para instalar iptables en sistemas basados en Red Hat
+install_iptables_redhat() {
+    sudo yum install -y iptables
+}
+
+# Verificar si iptables ya está instalado
+check_iptables_installed
+
+# Detectar el sistema operativo y llamar a la función de instalación correspondiente
+if [ -f /etc/debian_version ]; then
+    echo "Sistema basado en Debian detectado."
+    install_iptables_debian
+elif [ -f /etc/redhat-release ]; then
+    echo "Sistema basado en Red Hat detectado."
+    install_iptables_redhat
+else
+    echo "Sistema operativo no soportado."
+    exit 1
+fi
+
+echo "iptables se ha instalado correctamente."
+
   # IPv4 flush
   iptables -P INPUT ACCEPT
   iptables -P FORWARD ACCEPT
